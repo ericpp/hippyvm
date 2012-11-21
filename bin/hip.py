072d3a2147e25e4e4fdad790c71784c89705d4a0
Armin Rigo <arigo@tunes.org>
2012-11-21 23:47:55 +0100

    Add hip.py, an equivalent to py.py.  Limited right now.

---
+++ bin/hip.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Main read-eval-print loop for untranslated Hippy.
+"""
+
+import sys, os
+
+if __name__ == '__main__':
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from hippy.sourceparser import parse
+from hippy.astcompiler import compile_ast
+from hippy.interpreter import Interpreter, Frame
+from hippy.objspace import getspace
+
+
+def repl(argv):
+    if len(argv) > 1:
+        print "XXX arguments ignored"
+    space = getspace()
+    interp = Interpreter(space)
+    while True:
+        try:
+            line = raw_input("Hippy > ")
+        except EOFError:
+            print
+            break
+        try:
+            pc = parse(line)
+            bc = compile_ast(pc, space, 0)
+        except Exception, e:
+            print >> sys.stderr, '%s: %s' % (e.__class__.__name__, e)
+            continue
+        frame = Frame(space, bc)
+        try:
+            interp.interpret(space, frame, bc)
+        except Exception, e:
+            print e
+            pdb.post_mortem(sys.exc_info()[2])
+
+if __name__ == '__main__':
+    repl(sys.argv)
